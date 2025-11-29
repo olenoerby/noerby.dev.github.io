f649da2ffc9407f2ab4c2f78c2637ad703be851f
Ole Nørby <olenoerby@users.noreply.github.com>
2025-11-29 23:04:30 +0100

    Add basic HTML structure to index.php

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <style>
+            html {
+                text-align: center;
+                width: 100%;
+                height: 100%;
+            }
+            .main {
+                position: relative;
+                padding-top: 10%;
+            }
+            .footer {
+                position: absolute;
+                bottom: 10%;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="main">
+            <p>If you can not read this message,<br>then the server is down.</p>
+        </div>
+    </body>
+    <meta http-equiv="refresh" content="10" />
+    </div>
+<html>
